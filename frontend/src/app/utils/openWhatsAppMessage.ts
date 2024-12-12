ed95cd49d1f9fbae9e5f3d8a557306cf6b9fd5a3
Cleber Vasconcelos <crebs.vas@gmail.com>
2024-12-12 17:32:26 -0300

    format number before send message

--- frontend/src/app/utils/openWhatsAppMessage.ts
+++ frontend/src/app/utils/openWhatsAppMessage.ts
@@ -9,9 +9,10 @@ export function openWhatsAppMessage({
   title,
   message,
 }: OpenWhatsAppMessageProps) {
+  const phoneNumberFormatted = String(phoneNumber).replace(/\W/gm, '')
   const formattedMessage = `*${title}*\n\n${message}`
   const encodedMessage = encodeURIComponent(formattedMessage)
-  const whatsappLink = `https://wa.me/+55${phoneNumber}?text=${encodedMessage}`
+  const whatsappLink = `https://wa.me/+55${phoneNumberFormatted}?text=${encodedMessage}`
 
   window.open(whatsappLink, '_blank')
 }
